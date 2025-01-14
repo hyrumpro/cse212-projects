ef6d99783e5b1857049a8137f727692f2041f098
hyrumpa <hyrumpa@gmail.com>
2025-01-14 01:55:09 +0000

    First

---
+++ week01/code/Arrays.cs
@@ -0,0 +1,46 @@
+public static class Arrays
+{
+    /// <summary>
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    public static double[] MultiplesOf(double number, int length)
+    {
+        // Step 1: Create a new array of size 'length' to store the multiples
+        // Step 2: Initialize the first element with the starting number
+        // Step 3: Use a loop to fill the array with multiples
+        // Step 4: Return the completed array
+        
+        var result = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    ///
+    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // Step 1: Get the portion of the list that needs to move to the front
+        // Step 2: Get the portion of the list that needs to move to the back
+        // Step 3: Clear the original list
+        // Step 4: Add the rotated portions in the correct order
+        
+        int splitPoint = data.Count - amount;
+        var firstPart = data.GetRange(0, splitPoint);
+        var secondPart = data.GetRange(splitPoint, amount);
+        
+        data.Clear();
+        data.AddRange(secondPart);
+        data.AddRange(firstPart);
+    }
+}
